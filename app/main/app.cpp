b04db8ecdfde401ef09e405c837c10b66d0d63ca
Finn Hannon <f.hannon@netze-bw.de>
2025-05-12 21:45:24 +0200

    fix merge errors

--- app/main/app.cpp
+++ app/main/app.cpp
@@ -4,57 +4,45 @@
 #include <stdexcept>
 #include "pars.h"
 #include "simvm.h"
-//#include <ftxui/dom/elements.hpp>
-//#include <ftxui/screen/screen.hpp>
+#include "ui/document.cpp"
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 
 int main(int argc, char* argv[]) {
-    //using namespace ftxui;
+    using namespace ftxui;
 
     std::string fileName;
     if (argc > 1) {
         fileName = argv[1];
     } else {
-        fileName = "TPicSim1.LST"; // Default file
+        fileName = "TPicSim1.LST";
     }
 
     std::string filePath = "../testprog/" + fileName; 
 
-    try {
-        Parser parser;
-        int lineCount = 0;
-        short* parsedLines = parser.parse(filePath, lineCount);
-        std::vector<short> lines(parsedLines, parsedLines + lineCount);
-        for (const auto& line : lines) {
-            std::cout << line << std::endl;
-        }
+    // try {
+    //     Parser parser;
+    //     int lineCount = 0;
+    //     short* parsedLines = parser.parse(filePath, lineCount);
+    //     std::vector<short> lines(parsedLines, parsedLines + lineCount);
+    //     for (const auto& line : lines) {
+    //         std::cout << line << std::endl;
+    //     }
 
-        PicSimulatorVM vm;
-        vm.initialize(lines);
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        std::cin.get(); // Wait for user input before closing
-        return 1;
-    }
+    //     PicSimulatorVM vm;
+    //     vm.initialize(lines);
+    // } catch (const std::exception& e) {
+    //     std::cerr << "Error: " << e.what() << std::endl;
+    //     std::cin.get();
+    //     return 1;
+    // }
 
     // ui
-
-    /*
-    Element document = hbox({
-        text("left")   | border,
-        text("middle") | border | flex,
-        text("right")  | border,
-    });
-
-    auto screen = Screen::Create(
-        Dimension::Full(),       // Width
-        Dimension::Fit(document) // Height
-    );
-    Render(screen, document);
-    screen.Print();
-    */
-
+    auto screen = ScreenInteractive::Fullscreen();
+    auto document = Document();
+    screen.Loop(document);
     
     std::cin.get(); // Wait for user input before closing
     return 0;
-   
 }
